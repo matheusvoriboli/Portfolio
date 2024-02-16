0e54342e158d3e44843095a4b1247d199562a11b
Matheus Riboli <CX-Matheus.Riboli@glhomes.com>
2024-02-16 18:45:01 -0300

    fix: Contact Box width for larger screens

--- src/components/ContactBox.tsx
+++ src/components/ContactBox.tsx
@@ -8,7 +8,7 @@ type ContactBoxProps = {
 
 export function ContactBox({ text, icon, href }: ContactBoxProps) {
   return (
-    <a href={href} target='_blank' className='custom-child flex border-solid border-neutral-600 border-[1px] rounded w-full p-3 items-center gap-2 justify-center cursor-pointer md:w-1/3'>
+    <a href={href} target='_blank' className='custom-child flex border-solid border-neutral-600 border-[1px] rounded w-full p-3 items-center gap-2 justify-center cursor-pointer md:max-w-64'>
       {icon}
       <h1 className='text-neutral-300'>{text}</h1>
       <ArrowUpRight size={21} className='ml-auto text-neutral-300' />
