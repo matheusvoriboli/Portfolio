3b1ae0f1186cb4d08c54d2cdfff6ff3a2992bd5e
Matheus Riboli <CX-Matheus.Riboli@glhomes.com>
2024-02-22 22:54:39 -0300

    fix: Removed github pages deploy settings

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Header } from "./components/Header.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Portfolio">
+    <BrowserRouter>
       <Header />
       <App />
     </BrowserRouter>
